9a6ae4699ca2f7552612507357ff45a4b6584636
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-16 15:58:14 -0500

    added styling to about me section and added project images

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import FullSizeRender from '../img/FullSizeRender.jpg'
 import headShot from '../img/headshot.jpg'
-import Instagram from '../img/iconfinder_instagram_3999037.png'
-import LinkedIn from '../img/iconfinder_linkedin_3999038.png'
+import Instagram from '../img/iconfinder_38-instagram_1161954.png'
+import LinkedIn from '../img/iconfinder_linkedin_circle_black_107159.png'
+import Medium from '../img/iconfinder_108-medium_710953.png'
+import GitHub from '../img/iconfinder_github_circle_black_107161.png'
 
 class AboutMe extends Component {
 
@@ -19,13 +20,20 @@ class AboutMe extends Component {
         </div>
 
         <div>
-          <img src={Instagram} alt="Instagram" height="50px"/>
-          <img src={LinkedIn} alt="LinkedIn"height="50px"/>
+          <img src={Instagram} alt="Instagram" className="media-icon"/>
+          <img src={LinkedIn} alt="LinkedIn" className="media-icon"/>
+          <img src={Medium} alt="Medium Blog" className="media-icon"/>
+          <img src={GitHub} alt="GitHub" className="media-icon"/>
         </div>
 
         <div className="bio">
-          Hello! My name is McKenzie Fiege and I am a Full Stack Web Developer. I have experience with JavaScript, Ruby, Ruby on Rails, CSS, React and Redux. I love creating sleek and beautiful user interfaces. When i'm not coding, I enjoy taking photos, tending to my many plants and petting dogs.
+          <span className="bio-span">
+            Hello! My name is McKenzie Fiege and I am a Full Stack Web Developer. <br/>I have experience with JavaScript, Ruby, Ruby on Rails, CSS, React and Redux. <br/>
+            I love creating sleek and beautiful user interfaces. <br/>
+            When i'm not coding, I enjoy taking photos, tending to my many plants and petting dogs.
+          </span>
         </div>
+
       </div>
     )
   }
