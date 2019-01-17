694962fa78450aa702d40e9e3194cded706213ca
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-17 13:06:24 -0500

    styled project pages. Added descriptions, tech stacks

--- src/Components/NovelFinds.js
+++ src/Components/NovelFinds.js
@@ -6,6 +6,26 @@ class NovelFinds extends Component {
     return (
       <div className="project-page">
         <h2 className="h2-headers">Novel Finds</h2>
+        <p className="project-description">
+        Novel Finds is an app designed to keep track of what you're reading and to join book clubs in you're area.
+        This app exists to give book lovers a place to keep track of what they've read or what they want to read. For those that want to join a book club to connect with other book lovers, users can create and join book clubs.
+        Create an account/ Login with a username and password if an exisiting user
+        Search for books using the Google Books API, add a book from search which will appear in "Want to read" section
+        Each user has a "Read", "Currently Reading", and "Want to read" sections
+        For each of the user's book, users can click image for more information, leave a review, favorite, and change its category
+        Can filter books from A-Z, Z-A or by favorited books
+        Users can create a new book club or search for book clubs using their zip code
+        Can join a bookclub or leave a bookclub if already a member
+        On a bookclub's page, there is an information snippet including attendees, address, time, decription and name of the host. Bookclub pages also include a discussion forum where users can post comments about the bookclub.
+        Users have the ability to search for reviews by entering the title of a book
+        </p>
+        <h3 className="project-h3-techstack">Tech Stack</h3>
+        <div className="skill-list">
+          <span>React | </span>
+          <span> Ruby on Rails | </span>
+          <span> Redux | </span>
+          <span> Google Books API</span>
+        </div>
         <a href="https://github.com/mckenziefiege/book_club_frontend">GitHub</a>
         <a href="https://www.youtube.com/watch?v=7J5aMVx50gk&feature=youtu.be">Video Demo</a>
         <img src="https://media.giphy.com/media/2wV3AaFKqZ5C2n7xu8/giphy.gif" />
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,24 +8,26 @@ class Projects extends Component {
       <div className="project-section">
         <h2 className="h2-headers">Projects</h2>
           <div className="project-container">
-            <Link to="/plant-diary">
               <div className="project-card">
                 <p className="project-title">Plant Diary</p>
-                <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49955133-1db73a00-fed0-11e8-94e8-0f9b46ac0451.png" height="300px"/>
+                  <Link to="/plant-diary">
+                    <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49955133-1db73a00-fed0-11e8-94e8-0f9b46ac0451.png" height="300px"/>
+                  </Link>
               </div>
-            </Link><br />
-            <Link to="/turbo-typing">
+            <br />
               <div className="project-card">
                 <p className="project-title">Turbo Typing</p>
-                <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49956937-72f54a80-fed4-11e8-8e18-efea50917fdc.png" height="300px"/>
+                  <Link to="/turbo-typing">
+                    <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49956937-72f54a80-fed4-11e8-8e18-efea50917fdc.png" height="300px"/>
+                  </Link>
               </div>
-            </Link><br />
-            <Link to="/novel-finds">
+            <br />
               <div className="project-card">
                 <p className="project-title">Novel Finds</p>
-                <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49903782-4854b480-fe36-11e8-87c7-08d1befc9b04.png" height="300px"/>
+                  <Link to="/novel-finds">
+                    <img className="project-image" src="https://user-images.githubusercontent.com/38592156/49903782-4854b480-fe36-11e8-87c7-08d1befc9b04.png" height="300px"/>
+                  </Link>
               </div>
-            </Link>
           </div>
       </div>
     )
