82d50476a5697aa1f3ecec64e39b62fbbb056d9d
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-21 15:57:44 -0500

    added menu options to nav bar

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react'
 
 class Menu extends Component {
 
+  state = {
+    options: false
+  }
+
+  changeMenu = ()  => {
+    this.setState({
+      options: !this.state.options
+    })
+  }
+
+  renderOptions = () => {
+     return (<ul>
+      <li>About Me</li>
+      <li>Projects</li>
+      <li>Resume</li>
+      <li>Contact</li>
+    </ul>
+    )
+  }
+
   render() {
     return (
-      <div className="menu">
+      <div onClick={this.changeMenu}className="menu">
         Menu
+        {this.state.options ? this.renderOptions() : null}
       </div>
     )
   }
